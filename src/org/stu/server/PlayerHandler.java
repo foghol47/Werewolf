85f27270afdbe06958d0313b4b226cb67a2065d9
Alireza Jabbari <foghol47@gmail.com>
2021-06-11 20:01:27 +0430

    feat: add ready users and day phase

--- src/org/stu/server/PlayerHandler.java
+++ src/org/stu/server/PlayerHandler.java
@@ -1,5 +1,9 @@
 package org.stu.server;
 
+import org.stu.elements.Mayor;
+import org.stu.elements.Doctor;
+import org.stu.elements.Role;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,10 +16,16 @@ public class PlayerHandler implements Runnable{
     private BufferedReader in;
     private PrintStream out;
     private String userName;
+    private Role role;
+    private boolean isAlive;
+    private boolean isReady;
+    private Task task;
 
     public PlayerHandler(Server server, Socket socket){
         this.server = server;
         this.socket = socket;
+        isAlive = true;
+        isReady = false;
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintStream(socket.getOutputStream());
@@ -25,21 +35,93 @@ public class PlayerHandler implements Runnable{
         }
     }
 
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setTask(Task task){
+        this.task = task;
+    }
+
+    public void setRole(Role role){
+        this.role = role;
+    }
+
+    public Role getRole(){
+        return role;
+    }
 
 
     @Override
     public void run() {
-        initUserName();
+        switch (task){
+            case INIT_USERNAME:
+                initUserName();
+                break;
+            case GETTING_READY:
+                gettingReady();
+                break;
+            case DAY:
+                day();
+                break;
+        }
+
+
+
+
 
     }
 
+//    public synchronized void setStateWaiting(){
+//        if (!server.allPlayersFinished(this)){
+//            try {
+//                Thread.currentThread().wait();
+//            }
+//            catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    public void day(){
+        out.println("its day. talk to each other.");
+        while (true){
+            try {
+                String message = userName + ": ";
+                message += in.readLine();
+                if (!task.equals(Task.DAY))
+                    break;
+                server.sendMessageToAll(this, message, true);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void getRoles(){
+        out.println("your role is " + role.getClass().getSimpleName() );
+        if (role instanceof Mayor){
+            String doctorUserName = server.findRoleUserName(new Doctor());
+            out.println("Doctor is " + doctorUserName);
+        }
+    }
+
+    public void receiveMessage(String message){
+        out.println(message);
+    }
+
+    public void sendMessage(String message){
+        server.sendMessageToAll(this, message, true);
+    }
+
     public void initUserName(){
-        System.out.println("Yoodada");
+
         String userNameTemp = "";
         while (true){
             out.println("enter your username:");
             try {
-                userNameTemp = in.readLine();
+                userNameTemp = in.readLine().trim();
             }
             catch (IOException e){
                 e.printStackTrace();
@@ -53,4 +135,24 @@ public class PlayerHandler implements Runnable{
         server.addUserName(userName);
 
     }
+
+    public void gettingReady(){
+        out.println("if you ready enter \"start\"");
+        String input = "";
+        do {
+            try {
+                input = in.readLine().toLowerCase();
+                if (!task.equals(Task.GETTING_READY))
+                    break;
+                if (!input.equals("start"))
+                    out.println("wrong input. try again");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }while (!input.equals("start"));
+        isReady = true;
+        server.incrementReadyPlayers();
+    }
 }
+
--- src/org/stu/server/Server.java
+++ src/org/stu/server/Server.java
@@ -1,10 +1,17 @@
 package org.stu.server;
 
+import org.stu.elements.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Server {
     private int port;
@@ -13,12 +20,14 @@ public class Server {
     private ServerSocket serverSocket;
     private int currentPlayers;
     private int numberPlayers;
+    private int readyPlayers;
     private final String chatFileName = "chats.txt";
 
     public Server(int port){
         this.port = port;
         players = new ArrayList<>();
         currentPlayers = 0;
+        readyPlayers = 0;
         userNames = new ArrayList<>();
 
         try {
@@ -34,13 +43,26 @@ public class Server {
         System.out.println("enter number of players:");
         Scanner scanner = new Scanner(System.in);
         numberPlayers = scanner.nextInt();
+        ExecutorService pool = Executors.newCachedThreadPool();
 
         while (currentPlayers < numberPlayers){
             try{
                 Socket player = serverSocket.accept();
                 PlayerHandler newPlayer = new PlayerHandler(this, player);
                 players.add(newPlayer);
-                new Thread(newPlayer).start();
+//                Thread t = new Thread(newPlayer);
+                newPlayer.setTask(Task.INIT_USERNAME);
+//                t.start();
+//                Thread.sleep(1000);
+//                t.stop();
+//                pool.schedule(newPlayer, 5, TimeUnit.SECONDS);
+//                pool.submit(newPlayer);
+//                pool.shutdownNow();
+                pool.execute(newPlayer);
+//                pool.shutdownNow();
+//                pool.awaitTermination(1, TimeUnit.SECONDS);
+
+
 
                 currentPlayers++;
             }
@@ -48,26 +70,190 @@ public class Server {
                 e.printStackTrace();
             }
         }
+        pool.shutdown();
+        try {
+            pool.awaitTermination(30, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        //pool.shutdownNow();
+        gameLoop();
+
 
     }
 
-    public synchronized void incrementPlayer(){
-        currentPlayers++;
-        if (currentPlayers == numberPlayers)
-            notifyPlayers();
+    public void gameLoop(){
+        gettingReady();
+        createRoles();
+        while (!gameOver()){
+            day();
+            if (gameOver())
+                break;
+            voting();
+
+        }
+    }
+
+    public void voting(){
+
+    }
+//    public void setTask(Task task){
+//        for (PlayerHandler player: players){
+//            player.setTask(task);
+//        }
+//    }
+
+    public boolean gameOver(){
+        return mafiaNumber() == 0 || mafiaNumber() >= citizenNumber();
+    }
+
+    public int mafiaNumber(){
+        int count = 0;
+        for (PlayerHandler player: players){
+            if (player.getRole() instanceof Mafia)
+                count++;
+        }
+        return count;
+    }
+    public int citizenNumber(){
+        int count = 0;
+        for (PlayerHandler player: players){
+            if (player.getRole() instanceof Citizen)
+                count++;
+        }
+        return count;
+    }
+
+    public void day(){
+        ExecutorService pool = Executors.newCachedThreadPool();
+        for (PlayerHandler player: players){
+            player.setTask(Task.DAY);
+            pool.execute(player);
+        }
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(40, TimeUnit.SECONDS))
+                pool.shutdownNow();
+            sendMessageToAll(null, "Time over and day ended.", false);
+
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void gettingReady(){
+        ExecutorService pool = Executors.newCachedThreadPool();
+        for (PlayerHandler player: players){
+            player.setTask(Task.GETTING_READY);
+            pool.execute(player);
+        }
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(10, TimeUnit.SECONDS))
+                pool.shutdownNow();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createRoles(){
+        int mafiaNumber = numberPlayers / 3;
+        int citizenNumber = numberPlayers - mafiaNumber;
+        ArrayList<Role> roles = new ArrayList<>();
+
+//        roles.add(new GodFather());
+//        roles.add(new DrLecter());
+//        roles.add(new OrdinaryMafia());
+
+        roles.add(new Detective());
+        roles.add(new Doctor());
+//        roles.add(new Sniper());
+//        roles.add(new DieHard());
+//        roles.add(new Psychologist());
+        roles.add(new Mayor());
+        roles.add(new OrdinaryCitizen());
+
+        Collections.shuffle(roles);
+
+        for (PlayerHandler player: players){
+            player.setRole(roles.get(0));
+            roles.remove(0);
+            player.getRoles();
+        }
 
     }
 
-    public void notifyPlayers(){
-        for (PlayerHandler handler: players)
-            handler.notify();
+
+    public synchronized void sendMessageToAll(PlayerHandler except, String message, boolean savedInFile){
+        for (PlayerHandler player: players){
+            if (!player.equals(except))
+                player.receiveMessage(message);
+        }
+        if (savedInFile) {
+            try (FileOutputStream file = new FileOutputStream(chatFileName, true)) {
+                PrintStream writer = new PrintStream(file);
+                writer.println(message);
+                writer.close();
+            } catch (FileNotFoundException e) {
+                System.err.println("file not found");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public boolean isValidUserName(String userName){
-        return !players.contains(userName);
+    public String findRoleUserName(Role role){
+        String string = "";
+        for (PlayerHandler player: players){
+            if (player.getRole().getClass().equals(role.getClass()))
+                string = player.getUserName();
+
+        }
+        return string;
     }
 
-    public void addUserName(String userName){
+//    public synchronized void incrementPlayer(){
+//        currentPlayers++;
+//        if (currentPlayers == numberPlayers)
+//            notifyPlayers();
+//
+//    }
+
+//    public void notifyPlayers(){
+//        for (PlayerHandler handler: players)
+//            new Thread(handler).notify();
+//    }
+
+
+//    public boolean allPlayersFinished(PlayerHandler except){
+//        if (currentPlayers != numberPlayers)
+//            return false;
+//        for (PlayerHandler handler: players){
+//            if (handler.equals(except))
+//                continue;
+//            if (!handler.getTaskFinished())
+//                return false;
+//        }
+//        return true;
+//    }
+
+    public synchronized boolean isValidUserName(String userName){
+        for (String users: userNames){
+            if (users.equals(userName))
+                return false;
+        }
+        return true;
+    }
+
+    public synchronized void addUserName(String userName){
         userNames.add(userName);
+    }
+
+    public synchronized void incrementReadyPlayers(){
+        readyPlayers++;
+
     }
 }
