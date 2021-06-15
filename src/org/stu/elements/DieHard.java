7a5ae3ec1783749d5303157a7d5414114c24532f
Alireza Jabbari <foghol47@gmail.com>
2021-06-15 14:06:52 +0430

    feat: add options for night act

--- src/org/stu/elements/DieHard.java
+++ src/org/stu/elements/DieHard.java
@@ -1,4 +1,43 @@
 package org.stu.elements;
 
+/**
+ * The type Die hard.
+ *
+ * @author Alireza Jabbari
+ */
 public class DieHard extends Citizen{
+    private int remainInquiry = 2;
+    private boolean hasSaved = true;
+
+    /**
+     * Use inquiry.
+     */
+    public void useInquiry(){
+        remainInquiry--;
+    }
+
+    /**
+     * Have inquiry boolean.
+     *
+     * @return true if have inquiry, otherwise false
+     */
+    public boolean haveInquiry(){
+        return remainInquiry > 0;
+    }
+
+    /**
+     * Use shield.
+     */
+    public void useShield(){
+        hasSaved = false;
+    }
+
+    /**
+     * Have shield boolean.
+     *
+     * @return true if have shield, otherwise false
+     */
+    public boolean haveShield(){
+        return hasSaved;
+    }
 }
--- src/org/stu/elements/DrLecter.java
+++ src/org/stu/elements/DrLecter.java
@@ -1,4 +1,28 @@
 package org.stu.elements;
 
+/**
+ * The type Dr lecter.
+ *
+ * @author Alireza Jabbari
+ */
 public class DrLecter extends Mafia{
+    private boolean savedHimself = false;
+
+    /**
+     * Is saved himself boolean.
+     *
+     * @return true if saved himself, otherwise false
+     */
+    public boolean isSavedHimself(){
+        return savedHimself;
+    }
+
+    /**
+     * Set saved himself.
+     *
+     * @param savedHimself saved himself boolean
+     */
+    public void setSavedHimself(boolean savedHimself){
+        this.savedHimself = savedHimself;
+    }
 }
